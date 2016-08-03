f050f5ec45601c5553a83348542dc080e62e4090
s-takuya <takuyashiratake0622@icloud.com>
2016-08-03 15:12:04 +0900

    Display entries

---
+++ RssReader/Entry.swift
@@ -0,0 +1,15 @@
+//
+//  Entry.swift
+//  RssReader
+//
+//  Created by 白武卓也 on 8/3/16.
+//  Copyright © 2016 takuya shiratake. All rights reserved.
+//
+
+import Foundation
+
+struct Entry {
+    var title : String
+    var desc : String
+    var link : String
+}
