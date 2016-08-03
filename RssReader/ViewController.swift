6fdfb3562e238c6ab81db418477e735d8fdb2173
s-takuya <takuyashiratake0622@icloud.com>
2016-08-03 14:21:48 +0900

    Initial Commit

---
+++ RssReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RssReader
+//
+//  Created by 白武卓也 on 8/3/16.
+//  Copyright © 2016 takuya shiratake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
